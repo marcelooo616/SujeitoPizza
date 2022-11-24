68fab9f19619f6aae1a726a49f22877b43bd6c0a
marcelooo616 <marcelooo2014@hotmail.com>
2022-11-24 18:55:11 -0300

    :wrench: feat: Instalando Postgres

--- backand/src/routes/Routes.ts
+++ backand/src/routes/Routes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response) => {
     return res.json({ nome: "Sujeito Pizza" })
 
 })
