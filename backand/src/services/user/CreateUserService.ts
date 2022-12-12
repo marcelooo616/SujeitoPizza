e227753b16d2a370ff8e2943f04b88d82062958e
marcelooo616 <marcelooo2014@hotmail.com>
2022-12-12 13:18:42 -0300

    :bricks: feat: Cadastrando user

--- backand/src/services/user/CreateUserService.ts
+++ backand/src/services/user/CreateUserService.ts
@@ -10,15 +10,41 @@ interface UserRequest{
 class CreateUserService{
     async execute({ name, email, password }: UserRequest){
         
-        //Verificar se ele enviou o email
+        //Verificar se ele enviou um email
+        if(!email){
+            throw new Error("Email incorreto")
+        }
 
 
         //verificra se o email ja esta cadastrado na plataforma
+        const userAlreadyExixts = await prismaClient.user.findFirst({
+          where:{
+            email:email
+          }
+        })
+
+        if(userAlreadyExixts){
+          throw new Error("Este email ja existe ")
+        }
+
+        const user = await prismaClient.user.create({
+          data:{
+            name: name,
+            email: email,
+            password: password,
+          },
+          select:{
+            id:true,
+            name:true,
+            email:true,
+
+          }
+        })
 
 
 
 
-        return {  name: name }
+        return user;
     } 
 }
 export { CreateUserService };
